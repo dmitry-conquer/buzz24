5003d0e3680f3d4a6a0b48adab50cf75fb32ca87
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-08-11 14:14:50 +0300

    lang added

--- src/js/modules/translate.js
+++ src/js/modules/translate.js
@@ -1,23 +1,46 @@
 export const useTranslate = () => {
    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-   elementsToTranslate.forEach(element => {
-      // console.log(element);
+   const translationsURL = {
+      UK_UA: "https://api.npoint.io/38e0577f8bc9a1ef8354",
+      EN: "https://api.npoint.io/69ced25cec9eb8aa2f4c",
+      PL_PL: "https://api.npoint.io/32ad70126c24454d2b61",
+      RO_RO: "",
+      TR_TR: "",
+   }
+
+   const changeLangTrigers = document.querySelectorAll('.location-header__radio-input');
+   changeLangTrigers.forEach(item => {
+      item.addEventListener('change', () => {
+         if (item.checked) {
+            changeLang(item);
+         }
+      })
    })
-   async function getTranlsate() {
+
+   function changeLang(item) {
+      const translateLang = item.value;
+      getTranlsate(translateLang)
+
+   }
+
+   async function getTranlsate(translateLang) {
       try {
-        const response = await fetch("../public/ru.json");
+        const response = await fetch(translationsURL[translateLang] || translationsURL.UK_UA);
         const translations =  await response.json();
-        console.log(translations);
-      //   elementsToTranslate.forEach(element => {
-      //    const translateKey = element.getAttribute("data-translate-key");
-      //    const userLanguage = navigator.language; // Отримуємо мову користувача
+        elementsToTranslate.forEach(element => {
+         const translateKey = element.getAttribute("data-translate");
+         // const userLanguage = navigator.language; // Отримуємо мову користувача
   
-      //    // Знаходимо відповідний переклад для поточної мови користувача
-      //    const translation = translations[translateKey][userLanguage] || translations[translateKey]["en"];
-  
-      //    // Встановлюємо переклад у вміст елемента
-      //    element.textContent = translation;
-      //  });
+         const translation = translations[translateKey];
+
+         if (element.tagName === 'INPUT') {
+            element.placeholder = translation;
+        } else {
+            element.textContent = translation;
+        }
+
+         
+       });
       } catch (error) {
         console.log(error);
       }
